fd06224f61317c850720fc6082a98aacf4713da8
ayu <delta.dev@nexagroup.co.id>
2022-05-31 21:06:22 +0700

    fix api warta

--- application/controllers/api/Warta.php
+++ application/controllers/api/Warta.php
@@ -89,85 +89,93 @@ class Warta extends CI_Controller {
 			$alasan = isset($params['alasan']) ? $params['alasan'] : '';
 
 			if($nij && $approval){
-				$get_notif = $this->Main_Model->view_by_id('notif_approval_warta', ['flag' => 1, 'nij' => $nij, 'id_warta' => $id_warta]);
-				$kolom = $get_notif->kolom;
-				$tabel = $get_notif->tabel;
-				$warta = $this->Main_Model->view_by_id('warta', ['flag' => 1, 'id' => $id_warta]);
-				$datetime = explode(' ', $warta->tanggal);
-				$time = date('H:i', strtotime($datetime[1]));
+				$get_notif = $this->Main_Model->view_by_id('notif_approval_warta', ['flag' => 1, 'approval' => 0, 'nij' => $nij, 'id_warta' => $id_warta]);
+				
+				if($get_notif) {
+					$kolom = $get_notif->kolom;
+					$tabel = $get_notif->tabel;
+					$warta = $this->Main_Model->view_by_id('warta', ['flag' => 1, 'id' => $id_warta]);
+					$datetime = explode(' ', $warta->tanggal);
+					$time = date('H:i', strtotime($datetime[1]));
 
-				$cond = '';
-				if(trim($tabel) != 'warta') {
-					$cond = '_warta';
-				}
-
-				$this->Main_Model->process_data('notif_approval_warta', ['approval' => $approval, 'update_at' => date('Y-m-d H-i-s')], ['id' => $get_notif->id]);
-
-				// 0:new ; 1: reject ; 2:approve
-				if($approval == 2) {
-					$data = ['approval_'.trim($kolom) => $approval];
-				}else{
-					if($nij_kandidat) {
-						if($alasan){
-							$data = [
-										trim($kolom) => $nij_kandidat,
-										'approval_'.trim($kolom) => $approval,
-										trim($kolom).'_nij_reject' => $nij,
-										trim($kolom).'_reason_reject' => $alasan,
-									];
-
-						}
-
-						$fcm = get_fcm($nij_kandidat);
-						$nama = get_nama($nij);
-						$nama_kandidat = get_nama($nij_kandidat);
-
-						$data_notif = array(
-								'id_warta' => $id_warta,
-								'tgl_warta' => local_date_format($datetime[0]).' '.$time,
-								'posisi' => $get_notif->posisi,
-								'nij_kandidat' => $nij_kandidat,
-								'nama_kandidat' => $nama_kandidat,
-								'nama' => $nama,
-								'nij' => $nij,
-								'flag_approval' => 1
-							);
-
-						$this->Warta_Model->notif_reject_warta($fcm, $nama, $data_notif);
-
-						// $this->Main_Model->process_data('notif_approval_warta', ['nij' => $nij_kandidat],['id_warta' => $id_warta, 'posisi' => $posisi]);
-
-						$this->Main_Model->process_data('notif_approval_warta', [ 
-							'id_warta' => $id_warta,
-							'nij' => $nij_kandidat,
-							'tgl_notif' => $get_notif->tgl_notif,
-							'posisi' => $get_notif->posisi,
-							'kolom' => $kolom,
-							'tabel' => $tabel,
-							'flag' => 0
-
-						]);
-
-						$status = 200;
-		                $message = 'Data berhasil disimpan';
-		                $response = $data_notif;
-
-					}else{
-						$status = 400;
-						$message = 'Alasan / Kandidat Tidak Boleh Kosong';
+					$cond = '';
+					if(trim($tabel) != 'warta') {
+						$cond = '_warta';
 					}
+
+					$this->Main_Model->process_data('notif_approval_warta', ['flag' => 0, 'approval' => $approval, 'update_at' => date('Y-m-d H-i-s')], ['id' => $get_notif->id]);
+
+					// 0:new ; 1: reject ; 2:approve
+					if($approval == 2) {
+						$data = ['approval_'.trim($kolom) => $approval];
+					}else{
+						if($nij_kandidat) {
+							if($alasan){
+								$data = [
+											trim($kolom) => $nij_kandidat,
+											'approval_'.trim($kolom) => $approval,
+											trim($kolom).'_nij_reject' => $nij,
+											trim($kolom).'_reason_reject' => $alasan,
+										];
+
+							}
+
+							$fcm = get_fcm($nij_kandidat);
+							$nama = get_nama($nij);
+							$nama_kandidat = get_nama($nij_kandidat);
+
+							$data_notif = array(
+									'id_warta' => $id_warta,
+									'tgl_warta' => local_date_format($datetime[0]).' '.$time,
+									'posisi' => $get_notif->posisi,
+									'nij_kandidat' => $nij_kandidat,
+									'nama_kandidat' => $nama_kandidat,
+									'nama' => $nama,
+									'nij' => $nij,
+									'flag_approval' => 1
+								);
+
+							$this->Warta_Model->notif_reject_warta($fcm, $nama, $data_notif);
+
+							// $this->Main_Model->process_data('notif_approval_warta', ['nij' => $nij_kandidat],['id_warta' => $id_warta, 'posisi' => $posisi]);
+
+							$this->Main_Model->process_data('notif_approval_warta', [ 
+								'id_warta' => $id_warta,
+								'nij' => $nij_kandidat,
+								'tgl_notif' => $get_notif->tgl_notif,
+								'posisi' => $get_notif->posisi,
+								'kolom' => $kolom,
+								'tabel' => $tabel,
+								'flag' => 0
+
+							]);
+
+							$status = 200;
+			                $message = 'Data berhasil disimpan';
+			                $response = $data_notif;
+
+						}else{
+							$status = 400;
+							$message = 'Alasan / Kandidat Tidak Boleh Kosong';
+						}
+					}
+
+					$save = $this->Main_Model->process_data(trim($tabel), $data, ['id'.$cond => $id_warta]);
+
+					if($save){
+		                $status = 200;
+		                $message = 'Data berhasil disimpan';
+		                $response = $data;
+		            }else{
+		                $status = 404;
+		                $message = 'Gagal Menyimpan data!';
+		            }
+
+				}else{
+					$status = 400;
+					$message = 'Anda Sudah Melakukan Approval';
 				}
-
-				$save = $this->Main_Model->process_data(trim($tabel), $data, ['id'.$cond => $id_warta]);
-
-				if($save){
-	                $status = 200;
-	                $message = 'Data berhasil disimpan';
-	                $response = $data;
-	            }else{
-	                $status = 404;
-	                $message = 'Gagal Menyimpan data!';
-	            }
+				
 			}else{
 				$status = 400;
 				$message = 'NIJ / Approval Tidak Boleh Kosong';
